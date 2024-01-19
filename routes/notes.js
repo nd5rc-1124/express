446e2eb935c4dabd2a6666a7daf6ae1807862cf1
OdisanMacmini <odisan@OdisanMacmininoMac-mini.local>
2024-01-19 17:39:55 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+require('dotenv').config();
+
+// CORSミドルウェアを使用
+//app.use(cors());
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-// Access Information is Deleted
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
